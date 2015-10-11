49e1b0e2cb9baee408dbd97db73d6ac6cc197678
Alvaro Licea, Jr. <Senor_guero@msn.com>
2015-10-10 20:24:00 -0700

    Added function definitions & members: overloaded ops, c'tor, d'tor, and constant default variables.

--- Polynomial/poly_tests.cpp
+++ Polynomial/poly_tests.cpp
@@ -2,5 +2,5 @@
 
 int main() 
 {
-	std::cout << "ayye its the ";
+	std::cout << "Polynomial Tests";
 }
--- Polynomial/polynomial.h
+++ Polynomial/polynomial.h
@@ -5,7 +5,29 @@
 
 class Polynomial {
 public:
+	static const int DEFAULT_SIZE = 1;
+	static const int DEFAULT_VALUE = 1;
+
+	Polynomial();
+	~Polynomial();
+
+	int length() const;
+
+	Polynomial operator+(const Polynomial&) const;
+	Polynomial operator-(const Polynomial&) const;
+	Polynomial operator*(const Polynomial&) const;
+	Polynomial operator/(const Polynomial&) const;
+
+	Polynomial operator+=(const Polynomial&);
+	Polynomial operator-=(const Polynomial&);
+	Polynomial operator*=(const Polynomial&);
+	Polynomial operator/=(const Polynomial&);
+
+	bool operator==(const Polynomial&) const;
+	bool operator!=(const Polynomial&) const;
 protected: 
+	int* poly;
+	int size;
 };
 
 #endif
