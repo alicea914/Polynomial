47c467f9d9abdec50d1f346001c757a9b9a1187a
alicea914 <licea.alvaro.jr@gmail.com>
2015-10-13 17:47:51 -0700

    Changed C'tor to Constructor.

--- Polynomial/poly_tests.cpp
+++ Polynomial/poly_tests.cpp
@@ -1,14 +1,31 @@
+//-----------------------------------------------------------------------------
 #include "polynomial.h"
 
 int main() 
 {
-	std::cout << "Polynomial Tests";
-
-	// First test the C'tor and overloaded output operator
+	//Test objects. 
+	Polynomial* polynomial_array = new Polynomial[20];
 	Polynomial a;
-	std::cout << a << "/n";
+
+	std::cout << "~*~*~*~*__Polynomial Tests__*~*~*~*~\n";
+	std::cout << "Written by Alvaro Licea, Jr.\n";
+
+	// First test the Constructor and overloaded output operator
+	std::cout << "\nTesting the constructor, and cout operator...\n";
+	std::cout << "---------------------------------------";
+	std::cout << "----------------------------------------\n";
+	std::cout << "This is a Poly:\t\t\t" << a << "\n";
 
 	// Then test the accessors
-	std::cout << "The highest degree of this polynomial is: " << a.highest_degree() << "\n";
-	std:: cout << "The length of the array is " << a.length() << "\n";
+	std::cout << "\nTesting the degree, length, and is_zero accessors...\n";
+	std::cout << "---------------------------------------";
+	std::cout << "----------------------------------------\n";
+	std::cout << "The highest degree of this polynomial is:\t"
+			  << a.highest_degree() << "\n";
+	std::cout << "The length of the polynomial array is:\t\t" 
+		      << a.length() << "\n";
+	std::cout << "Is this polynomial a zero polynomial?\t\t" 
+		      << a.is_zero() << "\n";
+
 }
+//-----------------------------------------------------------------------------
