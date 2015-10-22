26a14f5a2e2aeebd29c5ef01a010550cff1b651f
Alvaro <licea.alvaro.jr@gmail.com>
2015-10-21 17:59:41 -0700

    Added tests for cin operator using b poly.

--- Polynomial/poly_tests.cpp
+++ Polynomial/poly_tests.cpp
@@ -6,25 +6,44 @@ int main()
   //Test objects. 
   Polynomial* polynomial_array = new Polynomial[20];
   Polynomial a;
+  Polynomial b;
 
   std::cout << "~*~*~*~*__Polynomial Tests__*~*~*~*~\n";
   std::cout << "Written by Alvaro Licea, Jr.\n";
 
-  // First test the Constructor and overloaded output operator
-  std::cout << "\nTesting the constructor, and cout operator...\n";
+  // test the Constructor and overloaded cin operator
+  std::cout << "\nTesting constructors...\n";
   std::cout << "---------------------------------------";
   std::cout << "----------------------------------------\n";
   std::cout << "This is a Poly:\t\t\t" << a << "\n";
 
-  // Then test the accessors
-  std::cout << "\nTesting the degree, length, and is_zero accessors...\n";
+  // test the overloaded cin operator
+  std::cout << "\nTesting cin operator (input required from user)...\n";
   std::cout << "---------------------------------------";
   std::cout << "----------------------------------------\n";
+  std::cin  >> b;
+  std::cout << "This is b Poly:\t\t\t" << a << "\n";
+
+  // test the accessors
+  std::cout << "\nTesting accessors...\n";
+  std::cout << "---------------------------------------";
+  std::cout << "----------------------------------------\n";
+  std::cout << "a Poly\n";
   std::cout << "The highest degree of this polynomial is:\t"
             << a.highest_degree() << "\n";
   std::cout << "The length of the polynomial array is:\t\t" 
             << a.length() << "\n";
   std::cout << "Is this polynomial a zero polynomial?\t\t" 
             << a.is_zero() << "\n";
+  
+  std::cout << "---------------------------------------";
+  std::cout << "----------------------------------------\n";
+  std::cout << "b Poly\n";
+  std::cout << "The highest degree of this polynomial is:\t"
+            << b.highest_degree() << "\n";
+  std::cout << "The length of the polynomial array is:\t\t" 
+            << b.length() << "\n";
+  std::cout << "Is this polynomial a zero polynomial?\t\t" 
+            << b.is_zero() << "\n";
 }
 //-----------------------------------------------------------------------------
